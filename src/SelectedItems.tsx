1c355ddb251ee96b662b7df8df51de51004962d7
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-03-28 01:56:21 +0900

    refactor(SelectedItems): Use common components

--- src/SelectedItems.tsx
+++ src/SelectedItems.tsx
@@ -1,6 +1,5 @@
-import { motion } from "framer-motion";
 import React from "react";
-import styled from "styled-components";
+import { SelectedItemPillBase, SelectedItemPillFadeInAnimator, SelectedItemPillLayoutAnimator, SelectedItemsContainer } from "./components/layout";
 import { pillVariants } from "./constants";
 import { useLayoutAnimation } from "./lib/hooks/useLayoutAnimation";
 import { Item } from "./types";
@@ -8,43 +7,6 @@ import { Item } from "./types";
 type Props = {
   items: Item[];
 };
-
-const SelectedItemsContainer = styled.div`
-  display: flex;
-  margin: 64px auto;
-  padding: 24px;
-  width: 720px;
-  overflow-x: scroll;
-  line-height: 48px;
-  border-radius: 4px;
-  background-color: rgba(0, 0, 0, 0.03);
-  &:empty {
-    height: 48px;
-  }
-`;
-// const SelectedItemPillLayoutAnimator = styled(motion.span)`
-const SelectedItemPillLayoutAnimator = styled.span`
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  transition-property: all;
-  & + & {
-    margin-left: 8px;
-  }
-`;
-
-const SelectedItemPillFadeInAnimator = styled(motion.span)``;
-
-const SelectedItemPillBase = styled.span`
-  background: rgba(0, 0, 0, 0.4);
-  font-size: 16px;
-  font-weight: 300;
-  line-height: 28px;
-  padding: 8px 24px;
-  border-radius: 9999vmax;
-  color: white;
-  word-break: keep-all;
-  cursor: pointer;
-`;
 
 const SelectedItemPill = ({ item }: { item: Item }) => {
   const animationTargetRef = useLayoutAnimation({

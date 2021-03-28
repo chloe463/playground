3dbf24e736faa21cda550d9ab41817e375360df0
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-03-29 00:58:24 +0900

    feat(ListToDetail): Add new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { LayoutAnimation } from "./pages/LayoutAnimation";
+import { ListToDetail } from "./pages/ListToDetail";
 
 const routes = [
   {
     key: "home",
     path: "/",
     component: LayoutAnimation,
+    exact: true,
   },
   {
     key: "layoutAnimation",
     path: "/layout-animation",
     component: LayoutAnimation,
+  },
+  {
+    key: "listToDetail",
+    path: ["/list-to-detail", "/list-to-detail/:id"],
+    component: ListToDetail,
   },
 ];
 
@@ -20,7 +27,7 @@ const AppRouter = () => {
       <Switch>
         {routes.map((route) => {
           return (
-            <Route key={route.key} path={route.path} component={route.component} />
+            <Route key={route.key} path={route.path} component={route.component} exact={route.exact} />
           );
         })}
       </Switch>

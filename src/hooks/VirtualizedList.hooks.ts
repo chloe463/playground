c429df5424d5d4ec71035118366e285f50b7416f
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-04-05 01:34:34 +0900

    feat(useVirtualizedList): Make it possible to pass variables to query

--- src/hooks/VirtualizedList.hooks.ts
+++ src/hooks/VirtualizedList.hooks.ts
@@ -1,8 +1,20 @@
 import { gql, useQuery } from "@apollo/client";
+import { useMemo } from "react";
+import { GetPostConnectionQuery, GetPostConnectionQueryVariables } from "./__generated__/GetPostConnectionQuery";
+import { PostFragment as Post } from "./__generated__/PostFragment";
+
+const PostFragment = gql`
+  fragment PostFragment on Post {
+    id
+    userId
+    title
+    body
+  }
+`;
 
 const GET_POSTS_QUERY = gql`
-  query GetPostConnectionQuery {
-    postConnection(first:10, after:"0") {
+  query GetPostConnectionQuery($first: Int, $after: String) {
+    postConnection(first: $first, after: $after) {
       pageInfo {
         hasNextPage
         hasPreviousPage
@@ -11,22 +23,38 @@ const GET_POSTS_QUERY = gql`
       }
       edges {
         node {
-          id
-          userId
-          title
-          body
+          ...PostFragment
         }
         cursor
       }
     }
   }
+  ${PostFragment}
 `;
 
+const DEFAULT_FETCH_SIZE = 10;
+const FIRST_CURSOR = "0";
+const EMPTY_POSTS: Post[] = [];
+
+const isPost = (node: Post | null | undefined): node is Post => Boolean(node);
+
 export const useVirtualizedList = () => {
-  const { data, loading } = useQuery(GET_POSTS_QUERY);
+  const { data, loading } = useQuery<GetPostConnectionQuery, GetPostConnectionQueryVariables>(GET_POSTS_QUERY, {
+    variables: {
+      first: DEFAULT_FETCH_SIZE,
+      after: FIRST_CURSOR,
+    },
+  });
+
+  const posts = useMemo<Post[]>(() => {
+    if (!data || !data.postConnection || !data.postConnection.edges) {
+      return EMPTY_POSTS;
+    }
+    return data.postConnection.edges.map((edge) => edge?.node).filter(isPost) || EMPTY_POSTS;
+  }, [data]);
 
   return {
-    posts: data,
+    posts,
     postsLoading: loading,
   };
 };

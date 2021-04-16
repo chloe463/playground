200b7d668552ca58c56ccbfa9a13f0ee68a9c071
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-04-17 02:12:54 +0900

    refactor(Post): Split component

--- src/pages/VirtualizedList.tsx
+++ src/pages/VirtualizedList.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { AppBase } from "../components/layout";
 import { PageHeader } from "../components/PageHeader";
+import { Post } from "../components/Post";
 import { useVirtualizedList } from "../hooks/VirtualizedList.hooks";
-
-const avatarUrl = "https://dummyimage.com/64x64/b3b3b3/ffffff";
 
 export const VirtualizedList = () => {
   const { posts } = useVirtualizedList();
@@ -17,19 +16,7 @@ export const VirtualizedList = () => {
           {posts.map((post) => {
             return (
               <PostListItem key={post.id}>
-                <DummyAvatar src={avatarUrl} />
-                <PostListItemContent>
-                  <ItemHeader>
-                    <ItemTitle>
-                      {post.id}. {post.title}
-                    </ItemTitle>
-                  </ItemHeader>
-                  <ItemBody>
-                    <ItemText>
-                      {post.body}
-                    </ItemText>
-                  </ItemBody>
-                </PostListItemContent>
+                <Post post={post} />
               </PostListItem>
             );
           })}
@@ -66,40 +53,4 @@ const PostListItem = styled.li`
   & + & {
     margin-top: 8px;
   }
-`;
-
-const DummyAvatar = styled.img`
-  border-radius: 50%;
-`;
-
-const PostListItemContent = styled.div`
-  margin-left: 16px;
-`;
-
-const ItemHeader = styled.header``;
-
-const ItemTitle = styled.h3`
-  margin: 0;
-  padding: 0;
-  font-size: 22px;
-  line-height: 32px;
-  color: rgba(0, 0, 0, 0.86);
-`;
-
-const ItemBody = styled.div`
-  margin-top: 4px;
-`;
-
-const ItemText = styled.p`
-  margin: 0;
-  padding: 0;
-  font-size: 14px;
-  line-height: 18px;
-  color: rgba(0, 0, 0, 0.56);
-
-  max-width: calc(1280px - 64px - 16px - 48px);
-  min-width: calc(720px - 64px - 16px - 48px);
-  overflow-x: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
 `;

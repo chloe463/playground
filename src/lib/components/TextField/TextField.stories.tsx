c4144c574e813f002113d58192e0af242151eb79
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-05-22 16:24:15 +0900

    storybook: Configure storybook and add story for TextField

---
+++ src/lib/components/TextField/TextField.stories.tsx
@@ -0,0 +1,23 @@
+import { action } from "@storybook/addon-actions";
+import { useState } from "react";
+import { TextField } from "./index";
+export default {
+  title: "TextField",
+};
+
+export const _Default = () => {
+  const [value, setValue] = useState("");
+  return (
+    <TextField
+      id="title"
+      label={"Title"}
+      name="title"
+      value={value}
+      autoComplete="off"
+      onChange={(s) => {
+        setValue(s);
+        action("onChange")(s);
+      }}
+    />
+  );
+};

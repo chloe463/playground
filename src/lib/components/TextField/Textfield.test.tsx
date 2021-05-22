ce573ab025ecedba24d838eb797f726e4cfa6b7a
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-05-22 18:17:05 +0900

    test(Textfield): Add test

---
+++ src/lib/components/TextField/Textfield.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { TextField } from "./index";
+
+describe("Textfield", () => {
+  it ("should render without crash", () => {
+    const { container } = render(<TextField label={"foo"} />);
+    const input = container.querySelector("input");
+    expect(input).toBeTruthy();
+  });
+});

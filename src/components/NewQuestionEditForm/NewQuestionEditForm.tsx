f45e9e51ccf6cce8f295cf1dabb619b8f57d2afd
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-06-16 02:10:35 +0900

    fix(NewQuestionEditForm): Add component

---
+++ src/components/NewQuestionEditForm/NewQuestionEditForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+
+};
+
+export const NewQuestionEditForm: React.VFC<Props> = (props) => {
+  return (
+    <Base>NewQuestionEditForm works!</Base>
+  );
+}
+
+const Base = styled.div``;
--- src/components/NewQuestionEditForm/NewQuestionEditFormContainer.tsx
+++ src/components/NewQuestionEditForm/NewQuestionEditFormContainer.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { useCrudPage } from "../../hooks/useCrudPage";
+import { NewQuestionEditForm } from "./NewQuestionEditForm";
 
 type Props = {
 
 };
 
 export const NewQuestionEditFormContainer: React.VFC<Props> = (props) => {
-  const { moveToQuestionEditPage, stateBetweenPages } = useCrudPage();
-  console.log({ stateBetweenPages });
   return (
-    <Base>New Component Works!</Base>
+    <NewQuestionEditForm />
   );
 }
 

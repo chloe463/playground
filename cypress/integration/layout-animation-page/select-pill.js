2956a2b62667f987af7783f1e88ebd1a9ca3c33e
Tsuyoshi HARA <chloe463+git@gmail.com>
2021-06-27 16:37:46 +0900

    test(LayoutAnimation): Add test for pill selection

--- cypress/integration/layout-animation-page/select-pill.js
+++ cypress/integration/layout-animation-page/select-pill.js
@@ -2,7 +2,6 @@ describe("LayoutAnimation page tests", () => {
   it("display 3 pills by default", () => {
     cy.visit("/layout-animation");
 
-    cy.wait(600);
     cy.get(".pill").should(($p) => {
       expect($p).to.have.length(3);
     });
@@ -11,12 +10,20 @@ describe("LayoutAnimation page tests", () => {
   it("display more pills by clicking add button", () => {
     cy.visit("/layout-animation");
 
-    cy.wait(1000);
     cy.get("button#add-button").click();
 
-    cy.wait(1000);
     cy.get(".pill").should(($p) => {
       expect($p.length).to.be.greaterThan(3);
+    });
+  });
+
+  it("selected pills will appear on gray box", () => {
+    cy.visit("/layout-animation");
+
+    cy.get(".pill").first().click();
+
+    cy.get(".selected-pill").should(($p) => {
+      expect($p).to.have.length(1);
     });
   });
 });

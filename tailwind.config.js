a0b2acf042a336e3e30789cab340b38914fb5100
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-02-27 16:50:23 +0900

    feat(tailwind): Add font family configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,42 @@ module.exports = {
   // ],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '160': '40rem',
+      },
+    },
+    fontFamily: {
+      heading: [
+        "Poppins",
+        '"Helvetica Neue"',
+        "Helvetica",
+        "Arial",
+        '"Hiragino Sans"',
+        "ヒラギノ角ゴシック",
+        '"Hiragino Kaku Gothic ProN"',
+        '"ヒラギノ角ゴ Pro W3"',
+        "Roboto",
+        "メイリオ",
+        "Meiryo",
+        '"ＭＳ Ｐゴシック"',
+        "sans-serif"
+      ],
+      body: [
+        "Lato",
+        '"Helvetica Neue"',
+        "Helvetica",
+        '"Hiragino Sans"',
+        '"ヒラギノ角ゴシック Pro"',
+        '"Hiragino Kaku Gothic Pro"',
+        "メイリオ",
+        "Meiryo", 
+        "Osaka",
+        '"ＭＳ Ｐゴシック"',
+        '"MS PGothic"',
+        "sans-serif",
+      ]
+    },
   },
   plugins: [],
 }

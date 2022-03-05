48285042560a2074fc10bbb2fae205d9a4bb9475
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-05 18:58:24 +0900

    refactor(tailwind): Replace files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
 
-const plugin = require("tailwindcss/plugin");
-
-const colors = require("./tailwind/colors");
-const fontSize = require("./tailwind/fontSize");
-const fontFamily = require("./tailwind/fontFamily");
+const colors = require("./tailwind/config/colors");
+const fontSize = require("./tailwind/config/fontSize");
+const fontFamily = require("./tailwind/config/fontFamily");
+const elevation = require("./tailwind/plugin/elevation");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
@@ -24,25 +23,6 @@ module.exports = {
     fontFamily,
   },
   plugins: [
-    plugin(function({ addUtilities, addComponents, e, prefix, config }) {
-      addUtilities(() => {
-        const shadow1 = `0 0 1px rgba(0, 0, 0, 0.02)`;
-        const shadow2 = (i) => {
-          if (i === 0) {
-            return `0 0 1px rgba(0, 0, 0, 0.1)`;
-          } else if (i < 8) {
-            return `0 ${i*0.5}px ${i*1.5}px rgba(0, 0, 0, 0.1)`;
-          } else if (i < 16) {
-            return `0 ${i*0.5}px ${(i*0.5)+8}px rgba(0, 0, 0, 0.1)`;
-          } 
-          return `0 ${i*0.5}px ${i}px rgba(0, 0, 0, 0.1)`;
-        };
-        const result = Array.from({ length: 40 }).map((_, i) => i).reduce((acc, i) => {
-          acc[`.elevation${i}`] = { boxShadow: `${shadow1}, ${shadow2(i)}` };
-          return acc;
-        }, {});
-        return result;
-      });
-    }),
-  ]
+    elevation,
+  ],
 }

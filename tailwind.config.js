be8dcd9dc7b97a9c2ee7ed6c2bf51ab6ecff0ee2
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-05 19:02:52 +0900

    fix: Delete unused line

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
-      boxShadow: {
-        elevation4: "0 2px 4px -1px rgb(0 0 0 / 20%), 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%)",
-      },
       transitionTimingFunction: {
         'in': 'cubic-bezier(0.3, 0.3, 0.3, 1)',
       },

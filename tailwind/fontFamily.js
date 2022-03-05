640ab3188e19bf5cc4a01967582954b0ffcf4372
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-05 18:30:10 +0900

    build(tailwind.config): Split configuration

---
+++ tailwind/fontFamily.js
@@ -0,0 +1,31 @@
+module.exports = {
+  heading: [
+    "Poppins",
+    '"Helvetica Neue"',
+    "Helvetica",
+    "Arial",
+    '"Hiragino Sans"',
+    "ヒラギノ角ゴシック",
+    '"Hiragino Kaku Gothic ProN"',
+    '"ヒラギノ角ゴ Pro W3"',
+    "Roboto",
+    "メイリオ",
+    "Meiryo",
+    '"ＭＳ Ｐゴシック"',
+    "sans-serif"
+  ],
+  body: [
+    "Lato",
+    '"Helvetica Neue"',
+    "Helvetica",
+    '"Hiragino Sans"',
+    '"ヒラギノ角ゴシック Pro"',
+    '"Hiragino Kaku Gothic Pro"',
+    "メイリオ",
+    "Meiryo", 
+    "Osaka",
+    '"ＭＳ Ｐゴシック"',
+    '"MS PGothic"',
+    "sans-serif",
+  ],
+};

2c8697df80bbebe59ca08fc302cf368002f6082f
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-08 21:59:04 +0900

    feat(useIsomorphicLayoutEffect): Add new hook

---
+++ src/hooks/useIsomarphicLayoutEffect.ts
@@ -0,0 +1,4 @@
+import { useEffect, useLayoutEffect } from "react";
+import { IS_SERVER } from "../common/isServer";
+
+export const useIsomorphicLayoutEffect =  IS_SERVER ? useEffect : useLayoutEffect;

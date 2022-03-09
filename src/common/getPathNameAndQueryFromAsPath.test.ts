98b3087bf4ea7cb004ed698238166453605f65ae
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-10 01:08:41 +0900

    test(getPathNameAndQueryFromAsPath): Add unittest

---
+++ src/common/getPathNameAndQueryFromAsPath.test.ts
@@ -0,0 +1,15 @@
+import { getPathNameAndQueryFromAsPath } from "./getPathNameAndQueryFromAsPath";
+
+describe("getPathNameAndQueryFromAsPath", () => {
+  it ("should split pathname", () => {
+    const { pathname, query } = getPathNameAndQueryFromAsPath("/path/to/some/page")
+    expect(pathname).toBe("/path/to/some/page");
+    expect(query.entries.length).toBe(0);
+  });
+
+  it ("should split pathname and search params", () => {
+    const { pathname, query } = getPathNameAndQueryFromAsPath("/path/to/some/page?foo=bar")
+    expect(pathname).toBe("/path/to/some/page");
+    expect(query.get("foo")).toBe("bar");
+  });
+});

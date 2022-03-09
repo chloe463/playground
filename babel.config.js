cc3ec12f223b6131bc4e4ecb50bdac893e29f61d
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-10 02:06:57 +0900

    build: Delete babel config in order to build with swc

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@babel/core').TransformOptions} */
-module.exports = {
-  presets: ["next/babel"],
-  plugins: [
-    "babel-plugin-macros",
-    "babel-plugin-styled-components",
-    ...(process.env.NODE_ENV === "production" ? [["react-remove-properties", { properties: ["data-cy"] }]] : []),
-  ],
-};
--- next.config.js
+++ next.config.js
@@ -6,5 +6,8 @@ module.exports = {
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
+    reactRemoveProperties: {
+      properties: ['^data-cy$']
+    }
   },
 };

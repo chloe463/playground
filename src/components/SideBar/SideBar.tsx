0ce6972777642c88dc0740325bf486133ae8e358
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-18 00:19:32 +0900

    fix(SideBar): Fix current page item color

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -14,10 +14,10 @@ type Props = {
 export const SIDEBAR_WIDTH = 280;
 
 const PAGES: Page[] = [
-  {
-    name: "home",
-    url: "/",
-  },
+  // {
+  //   name: "home",
+  //   url: "/",
+  // },
   {
     name: "layoutAnimation",
     url: "/layout-animation",
@@ -58,8 +58,8 @@ export const SideBar: React.VFC<Props> = (props) => {
                       visited:text-black-alpha500 visited:no-underline
                     `,
                     {
-                      "text-black-alpha800": props.currentPathName === page.url,
-                      "text-black-alpha500": props.currentPathName !== page.url
+                      "text-black-alpha800": props.currentPathName.startsWith(page.url),
+                      "text-black-alpha500": !props.currentPathName.startsWith(page.url)
                     },
                   )}
                 >
@@ -67,8 +67,8 @@ export const SideBar: React.VFC<Props> = (props) => {
                     classnames(
                       "block w-6 h-6 rounded mr-4 transition-all duration-200 ease-out",
                       {
-                        "bg-black-alpha500": props.currentPathName === page.url,
-                        "bg-black-alpha400": props.currentPathName !== page.url,
+                        "bg-black-alpha500": props.currentPathName.startsWith(page.url),
+                        "bg-black-alpha400": !props.currentPathName.startsWith(page.url)
                       }
                     )
                   } />

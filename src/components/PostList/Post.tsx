1353d129940803ab6550c89856d8bfcf59ce622a
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-26 01:16:45 +0900

    fix(Post): Fix link

--- src/components/PostList/Post.tsx
+++ src/components/PostList/Post.tsx
@@ -11,27 +11,24 @@ type Props = {
 
 export const Post: React.VFC<Props> = ({ post }) => {
   return (
-    <Link
-      href={{
-        pathname: `/virtualized-list/${post.id}`,
-      }}
-      shallow={true}
-    >
-      <a className="flex items-center mb-2 py-4 px-6 transition duration-75 no-underline" data-cy="link-to-post-detail">
-        <motion.img src={AVATAR_URL} className="rounded-full" width={64} height={64} />
-        <div className="ml-4 w-[calc(100%-80px)]">
-          <header>
-            <motion.h3 className="text-subheading text-gray-800">
-              {post.id}. {post.title}
-            </motion.h3>
-          </header>
-          <motion.div className="mt-1">
-            <p className="text-body2 text-gray-500 max-w-[1154px] min-w-[592px] overflow-x-hidden text-ellipsis whitespace-nowrap">
-              {post.body}
-            </p>
-          </motion.div>
-        </div>
-      </a>
-    </Link>
+    <div className="flex items-center mb-2 py-4 px-6 transition duration-75 hover:bg-black-alpha50" data-cy="link-to-post-detail">
+      <motion.img src={AVATAR_URL} className="rounded-full" width={64} height={64} />
+      <div className="ml-4 w-[calc(100%-80px)]">
+        <header>
+          <Link href={{ pathname: `/virtualized-list/${post.id}` }} shallow>
+            <a className="hover:underline">
+              <h3 className="inline text-subheading text-gray-800">
+                {post.id}. {post.title}
+              </h3>
+            </a>
+          </Link>
+        </header>
+        <motion.div className="mt-1">
+          <p className="text-body2 text-gray-500 max-w-[1154px] min-w-[592px] overflow-x-hidden text-ellipsis whitespace-nowrap">
+            {post.body}
+          </p>
+        </motion.div>
+      </div>
+    </div>
   );
 }

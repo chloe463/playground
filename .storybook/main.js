70e7eb4bbf8c558db543b695168d10aceeb18d03
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-27 16:31:52 +0900

    fix(storybook): Fix storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,15 +6,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
   ]
 }

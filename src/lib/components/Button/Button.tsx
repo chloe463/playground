54fe0dc8493f458e42bc1bb38fdace2d9bcf568e
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-03-27 21:12:12 +0900

    feat(elevation): Add util function to get elevation

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { colors } from "../../styles";
+import { getElevation } from "../elevation";
 
 export const BaseButton = styled.button`
   position: relative;
@@ -18,7 +19,7 @@ export const BaseButton = styled.button`
   border-radius: 9999vmax;
   cursor: pointer;
   transition: all 200ms cubic-bezier(0.3, 0.3, 0.3, 1);
-  box-shadow: 0 2px 4px -1px rgb(0 0 0 / 20%), 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%);
+  box-shadow: ${getElevation(4)};
   overflow: hidden;
 
   &:after {
@@ -31,7 +32,7 @@ export const BaseButton = styled.button`
   }
 
   &:hover {
-    box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%), 0 8px 10px 1px rgb(0 0 0 / 14%), 0 3px 14px 2px rgb(0 0 0 / 12%);
+    box-shadow: ${getElevation(8)};
     &:after {
       background-color: ${colors.blackAlpha50};
     }
@@ -53,7 +54,7 @@ export const PrimaryButton = styled(BaseButton)`
   color: ${colors.white};
 
   &:hover {
-    box-shadow: 0 5px 5px -3px rgb(0 0 0 / 20%), 0 8px 10px 1px rgb(0 0 0 / 14%), 0 3px 14px 2px rgb(0 0 0 / 12%);
+    box-shadow: ${getElevation(8)};
     &:after {
       background-color: ${colors.whiteAlpha50};
     }

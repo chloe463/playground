d41bde6298ee5a22c5fa480da6373f2fc0bbc9c7
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-04-02 01:13:36 +0900

    feat(ModalDialog): Add new lib component

---
+++ src/lib/components/ModalDialog/ModalDialog.stories.tsx
@@ -0,0 +1,42 @@
+import {
+  OverlayContainer,
+  OverlayProvider
+} from "@react-aria/overlays";
+import { useOverlayTriggerState } from "@react-stately/overlays";
+import { ModalDialog } from "./ModalDialog";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "lib/ModalDialog",
+};
+
+export const _Default = () => {
+  const state = useOverlayTriggerState({});
+  return (
+    <OverlayProvider>
+      <button onClick={() => state.open()}>
+        toggle
+      </button>
+      {state.isOpen && (
+        <OverlayContainer>
+          <ModalDialog
+            title={"Title"}
+            isOpen
+            onClose={state.close}
+            isDismissable
+          >
+            <div className="mt-4 mx-10">
+              Contents goes here.
+            </div>
+            <div className="flex justify-end mt-8 mb-5 px-8">
+              <button onClick={() => state.close()}>
+                close
+              </button>
+            </div>
+          </ModalDialog>
+        </OverlayContainer>
+      )}
+    </OverlayProvider>
+  );
+};
+

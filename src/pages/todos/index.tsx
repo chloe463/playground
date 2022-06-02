539329b6044cb5e92faf1b1acbd4ce8b41f50aca
Tsuyoshi HARA <chloe463+git@gmail.com>
2022-06-02 23:54:20 +0900

    feat(todos): Add todos index page

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,13 +1,38 @@
+import Link from "next/link";
 import React from "react";
+import { appBaseStyle } from "../../components/layout";
+import { PageHeader } from "../../components/PageHeader";
 
-type Props = {
+type Props = {};
 
-};
+const PAGES = [
+  {
+    url: "/react-hooks",
+    label: "React hooks",
+  },
+];
 
-const Component: React.VFC<Props> = (props) => {
+const Component: React.VFC<Props> = () => {
   return (
-    <Base>New Component Works!</Base>
+    <div className={appBaseStyle}>
+      <PageHeader title={"Todo list"} />
+      <div className="block px-6 mt-4">
+        <ul>
+          {PAGES.map((page) => {
+            return (
+              <li key={page.url}>
+                <Link href={`/todos/${page.url}`}>
+                  <a className="text-heading3 text-black-alpha800 hover:text-black-alpha500 hover:underline">
+                    {page.label}
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
-}
+};
 
 export default Component;
